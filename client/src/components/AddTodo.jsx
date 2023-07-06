1e6847e0641128cb23e5828132cce8a863ebc5d3
Simon Chanzu <chanzusimon6@gmail.com>
2023-07-06 10:46:17 +0300

    upload of additions

--- client/src/components/AddTodo.jsx
+++ client/src/components/AddTodo.jsx
@@ -1,43 +1,38 @@
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import Axios from "axios";
-import './addtodo.css';
-import { apiDomain } from "../utils/utils";
-import { useContext } from "react";
-import { Context } from "../context/userContext/Context";
+import { useDispatch, useSelector } from "react-redux"; // Import Redux hooks
+import { addTodo } from "../redux/todoSlice"; // Import the Redux action
+import "./addtodo.css";
 
 export default function AddTodo() {
-    const { user } = useContext(Context)
-    const schema = yup.object().shape({
-        description: yup.string().required("description is required"),
-    });
+  const user = useSelector((state) => state.user.user); // Access the user state from Redux
+  const dispatch = useDispatch(); // Access the dispatch function
 
-    const { register, handleSubmit, formState: { errors }, reset } = useForm({
-        resolver: yupResolver(schema),
-    });
-    const onSubmit = (data) => {
-        Axios.post(`${apiDomain}/todos`, data,
-            { headers: { "Authorization": `${user.token}` } })
-            .then((response) => {
-                response.data.message && alert(response.data.message)
-                reset();
-            })
-            .catch(({ response }) => {
-                alert(response.data.error)
-            });
-    };
-    return (
-        <div className="formWrapper">
-            <form onSubmit={handleSubmit(onSubmit)} className="Form" >
-                <textarea
-                    {...register("description")}
-                    name="description"
-                    id="description"
-                ></textarea>
-                <p>{errors.description?.message}</p>
-                <input className="submitBtn" type="submit" value="save" />
-            </form>
-        </div>
-    )
+  const schema = yup.object().shape({
+    description: yup.string().required("description is required"),
+  });
+
+  const { register, handleSubmit, formState: { errors }, reset } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    dispatch(addTodo({ description: data.description, token: user.token })); // Dispatch the addTodo action
+    reset();
+  };
+
+  return (
+    <div className="formWrapper">
+      <form onSubmit={handleSubmit(onSubmit)} className="Form">
+        <textarea
+          {...register("description")}
+          name="description"
+          id="description"
+        ></textarea>
+        <p>{errors.description?.message}</p>
+        <input className="submitBtn" type="submit" value="save" />
+      </form>
+    </div>
+  );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,32 +1,46 @@
-import { Link } from "react-router-dom"
-import { FaHome, FaInfoCircle, FaBook, FaSignOutAlt } from "react-icons/fa"
-import './header.css'
-import { useContext } from 'react'
-import { Context } from "../context/userContext/Context";
+import { Link } from "react-router-dom";
+import { FaHome, FaInfoCircle, FaBook, FaSignOutAlt } from "react-icons/fa";
+import "./header.css";
+import { useSelector, useDispatch } from "react-redux"; 
+import { logout } from "../redux/userSlice"; 
 import { useNavigate } from "react-router-dom";
-function Header() {
-    const { user, dispatch } = useContext(Context);
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        dispatch({ type: "LOGOUT" });
-        navigate("/");
-    };
-    return (
-        <div className="header" >
-            <div className="header-Wrapper" style={{}}>
-                <Link to='/' style={{ color: "brown" }}><FaHome id="icons" />Home</Link>
-                <Link to='/register' style={{ color: "purple" }}><FaInfoCircle id="icons" /> Register</Link>
-                {user && (
-                    <>
-                        <Link to='/todos' style={{ color: "green" }}><FaBook id="icons" /> Todos</Link>
-                        <Link onClick={handleLogout} style={{ color: "red" }}><FaSignOutAlt id="icons" /> Logout</Link>
-                    </>
-                )
 
-                }
-            </div>
-        </div>
-    )
+function Header() {
+  const user = useSelector((state) => state.user.user); 
+  const dispatch = useDispatch(); 
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logout()); 
+    navigate("/");
+  };
+
+  return (
+    <div className="header">
+      <div className="header-Wrapper">
+        <Link to="/" style={{ color: "brown" }}>
+          <FaHome id="icons" />
+          Home
+        </Link>
+        <Link to="/register" style={{ color: "purple" }}>
+          <FaInfoCircle id="icons" />
+          Register
+        </Link>
+        {user && (
+          <>
+            <Link to="/todos" style={{ color: "green" }}>
+              <FaBook id="icons" />
+              Todos
+            </Link>
+            <Link onClick={handleLogout} style={{ color: "red" }}>
+              <FaSignOutAlt id="icons" />
+              Logout
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
